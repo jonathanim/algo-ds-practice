1c0c84c91bb2c9bc606eb6e5562db1422187b919
unknown <imjonathan.work@gmail.com>
2022-07-08 15:16:38 -0700

    Linked list shift, pop, push

---
+++ linklist.js
@@ -0,0 +1,71 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  //insert at the end
+  push(value) {
+    let node = new Node(value);
+
+    if (this.head == null) {
+      this.head = node;
+      this.tail = this.head;
+    } else {
+      this.tail.next = node;
+      this.tail = node;
+    }
+    this.length++;
+    return this;
+  }
+
+  //removes at the end
+  pop() {
+    if (!this.head) {
+      return undefined;
+    }
+
+    let current = this.head;
+    let newTail = current;
+
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    return this.current;
+  }
+
+  shift() {
+    if (!this.head) {
+      return undefined;
+    }
+    let removed = this.head;
+
+    this.head = removed.next;
+    this.length--;
+    return removed;
+  }
+}
+
+let ll = new LinkedList();
+
+ll.push("henry");
+ll.push("harriot");
+ll.push("james");
+ll.push("dan");
+ll.push("jon");
+
+ll.pop();
+ll.pop();
+ll.shift();
+console.log(ll);
